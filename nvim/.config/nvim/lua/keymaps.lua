22ff3a2cb9ec80ff37c182140cbf5138ba5c3c4e
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2023-09-03 23:53:27 +0300

    Minor improvments on telescope keymaps

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -17,15 +17,16 @@ keymap.set("n", "<leader>t[", ":tabp<CR>")
 keymap.set("n", "<leader>t]", ":tabn<CR>")
 
 -- telescope
-keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
-keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
-keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
-keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
-keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+local builtin = require "telescope.builtin"
+keymap.set("n", "<leader>ff", builtin.find_files, {})
+keymap.set("n", "<leader>fc", builtin.grep_string, {})
+keymap.set("n", "<leader>fg", builtin.live_grep, {})
+keymap.set("n", "<leader>fb", builtin.buffers, {})
+keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
--- telescope git commands (not on youtube nvim video)
-keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
-keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
-keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
-keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
+keymap.set("n", "<leader>gf", builtin.git_files, {})
+keymap.set("n", "<leader>gc", builtin.git_commits, {})
+keymap.set("n", "<leader>gh", builtin.git_bcommits, {})
+keymap.set("n", "<leader>gb", builtin.git_branches, {})
+keymap.set("n", "<leader>gs", builtin.git_status, {})
 
