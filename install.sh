c222b76651efab75e903af256340960429ce331e
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2023-09-03 10:26:02 +0300

    better ignore

--- install.sh
+++ install.sh
@@ -1,10 +1,27 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-for item in ./*; do
-    package=$(basename $item)
-    if [ -d "$package" ]; then
-        stow --verbose --restow $package
-    fi
-done
+
+stow_packages()
+{
+    stow_cmd=${1:-restow}
+
+    for item in ./*; do
+        package=$(basename $item)
+        if [ -d "$package" ]; then
+            stow --verbose "--$stow_cmd" $package
+        fi
+    done
+}
+
+action=${1:-}
+case "$action" in
+    stow|restow|delete)
+        echo "Apllying $action to all packages..."
+        stow_packages $action
+        ;;
+    *)
+        echo "ERROR: Provide action '$action' is not valid."
+        ;;
+esac
 
