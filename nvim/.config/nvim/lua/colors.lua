823fa88b13558d9f60ea7b9879fa8b3426997015
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2023-10-01 22:42:06 +0300

    Even more

--- nvim/.config/nvim/lua/colors.lua
+++ nvim/.config/nvim/lua/colors.lua
@@ -4,5 +4,5 @@ vim.opt.termguicolors = true
 vim.cmd [[colorscheme nightfly]]
 vim.opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 vim.opt.showmode = false -- lightline already shows the current mode
-vim.g.lightline = { colorscheme = "one" }
+vim.g.lightline = { colorscheme = "nord" }
 
