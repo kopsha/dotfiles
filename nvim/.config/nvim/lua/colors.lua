feea1e82d19a591bb38132edc379512ce8eb17a5
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2024-04-13 18:18:35 +0300

    Just started

--- nvim/.config/nvim/lua/colors.lua
+++
@@ -1,8 +0,0 @@
--- turn on termguicolors for nightfly colorscheme to work
--- (have to use iterm2 or any other true color terminal)
-vim.opt.termguicolors = true
-vim.cmd [[colorscheme nightfly]]
-vim.opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-vim.opt.showmode = false -- lightline already shows the current mode
-vim.g.lightline = { colorscheme = "nord" }
-
--- nvim/.config/nvim/lua/config-plugins/init.lua
+++
@@ -1,8 +0,0 @@
-local config_plugins = {}
-
--- list all plugins that need to be configured
-config_plugins.lsp = require "config-plugins.lsp"
-config_plugins.treesitter = require "config-plugins.treesitter"
-config_plugins.telescope = require "config-plugins.telescope"
-
-return config_plugins
