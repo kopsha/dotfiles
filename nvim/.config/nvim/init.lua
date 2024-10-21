45a8dec455d5bce9dfe71188dcd4eb0a28a05c98
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2024-10-21 10:31:16 +0300

    Added text objects for classes, functions, arguments, comments.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -264,7 +264,34 @@ require("lazy").setup({
 			require("nvim-treesitter.configs").setup(opts)
 		end,
 	},
-	{ "nvim-treesitter/nvim-treesitter-textobjects" },
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			opts = {
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["aa"] = "@parameter.outer",
+							["ia"] = "@parameter.inner",
+							["ab"] = "@block.outer",
+							["ib"] = "@block.inner",
+							["ac"] = "@conditional.outer",
+							["ic"] = "@conditional.inner",
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["am"] = "@comment.outer",
+							["im"] = "@comment.inner",
+							["ao"] = "@class.outer",
+							["io"] = "@class.inner",
+						},
+					},
+				},
+			},
+		},
+	},
 
 	{ -- Autocompletion
 		"hrsh7th/nvim-cmp",
@@ -370,23 +397,7 @@ require("lazy").setup({
 			},
 		},
 		opts = {
-			notify_on_error = false,
-			format_on_save = function(bufnr)
-				-- Disable "format_on_save lsp_fallback" for languages that don't
-				-- have a well standardized coding style. You can add additional
-				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = true, cpp = true }
-				local lsp_format_opt
-				if disable_filetypes[vim.bo[bufnr].filetype] then
-					lsp_format_opt = "never"
-				else
-					lsp_format_opt = "fallback"
-				end
-				return {
-					timeout_ms = 500,
-					lsp_format = lsp_format_opt,
-				}
-			end,
+			notify_on_error = true,
 			formatters_by_ft = {
 				lua = { "stylua" },
 				python = { "isort", "ruff format" },
