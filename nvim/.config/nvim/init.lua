9a2d6abcf6f66b3fe509c3e482ad5b6b9727d5a4
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2026-04-07 16:45:01 +0300

    More fixing the nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -85,7 +85,6 @@ require("lazy").setup({
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
 
 			-- Useful status updates for LSP.
-			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
 			{ "j-hui/fidget.nvim", opts = {} },
 
 			-- Allows extra capabilities provided by nvim-cmp
@@ -407,12 +406,19 @@ require("lazy").setup({
 			"saadparwaiz1/cmp_luasnip",
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-buffer",
 		},
 		config = function()
-			-- See `:help cmp`
 			local cmp = require("cmp")
 			local luasnip = require("luasnip")
+
 			luasnip.config.setup({})
+
+			local has_words_before = function()
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
 
 			cmp.setup({
 				snippet = {
@@ -420,50 +426,80 @@ require("lazy").setup({
 						luasnip.lsp_expand(args.body)
 					end,
 				},
+
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+
 				completion = { completeopt = "menu,menuone,noinsert" },
 
-				-- For an understanding of why these mappings were
-				-- chosen, you will need to read `:help ins-completion`
-				--
-				-- No, but seriously. Please read `:help ins-completion`, it is really good!
 				mapping = cmp.mapping.preset.insert({
 					["<C-n>"] = cmp.mapping.select_next_item(),
 					["<C-p>"] = cmp.mapping.select_prev_item(),
 
-					-- Scroll the documentation window [b]ack / [f]orward
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 
-					-- Accept ([y]es) the completion.
-					--  This will auto-import if your LSP supports it.
-					--  This will expand snippets if the LSP sent a snippet.
 					["<C-y>"] = cmp.mapping.confirm({ select = true }),
-
-					-- Manually trigger a completion from nvim-cmp.
 					["<C-Space>"] = cmp.mapping.complete({}),
 
+					-- Snippet jump
 					["<C-l>"] = cmp.mapping(function()
 						if luasnip.expand_or_locally_jumpable() then
 							luasnip.expand_or_jump()
 						end
 					end, { "i", "s" }),
+
 					["<C-h>"] = cmp.mapping(function()
 						if luasnip.locally_jumpable(-1) then
 							luasnip.jump(-1)
 						end
 					end, { "i", "s" }),
 
-					-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-					--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+					-- Tab completion
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_locally_jumpable() then
+							luasnip.expand_or_jump()
+						elseif has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.locally_jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
+
+				formatting = {
+					format = function(entry, item)
+						local menu = {
+							nvim_lsp = "[LSP]",
+							luasnip = "[Snip]",
+							buffer = "[Buf]",
+							path = "[Path]",
+							lazydev = "[Lua]",
+						}
+						item.menu = menu[entry.source.name]
+						return item
+					end,
+				},
+
 				sources = {
-					{
-						name = "lazydev",
-						-- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
-						group_index = 0,
-					},
+					{ name = "lazydev", group_index = 0 },
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
+					{ name = "buffer" },
 					{ name = "path" },
 				},
 			})
@@ -503,10 +539,7 @@ require("lazy").setup({
 		},
 	},
 
-	"honza/vim-snippets",
-
 	"tpope/vim-fugitive",
-	"tpope/vim-endwise",
 	"tpope/vim-vinegar",
 	"tpope/vim-surround",
 
@@ -537,14 +570,6 @@ require("lazy").setup({
 
 	-- These mini plugins are not all that mini
 	{ "echasnovski/mini.splitjoin", version = "*", opts = {} },
-	{
-		"echasnovski/mini.statusline",
-		version = "*",
-		opts = { use_icons = false },
-	},
-
-	"https://github.com/Raimondi/delimitMate",
-
 	{
 		"bluz71/vim-nightfly-colors",
 		name = "nightfly",
@@ -579,8 +604,8 @@ require("lazy").setup({
 				debounce = 75, -- delay before suggestions appear
 				keymap = {
 					accept = "<C-y>",
-					next = "<C-n>",
-					prev = "<C-p>",
+					next = "<M-n>",
+					prev = "<M-p>",
 					dismiss = "<C-e>",
 				},
 			},
